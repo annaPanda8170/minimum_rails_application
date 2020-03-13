c99035662c577051d4cdd02da00faca7ee6f1df1
annaPanda8170 <handa_ryohei@yahoo.co.jp>
2020-03-14 03:06:47 +0900

    ③2ページ目用コントローラ

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   get 'messages/index'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+  }
+  devise_scope :user do
+    get 'cellphones', to: 'users/registrations#new_cellphone'
+    post 'cellphones', to: 'users/registrations#create_cellphone'
+  end
   root "messages#index"
 end
